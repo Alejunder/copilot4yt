4003277de7cd1a4a58e208644edc1280401c8c6c
Alejunder <chechis.cole@gmail.com>
2026-03-16 22:51:36 +0100

    useEffect agregado

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SoftBackdrop from "./SoftBackdrop";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
